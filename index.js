3557c918682970970028b230d974f0fc20b27ad6
“c-moX” <cmoxsh@gmail.com>
2021-07-09 17:34:10 +0600

    domainamex server code

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectID;
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// Mongodb connection:
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xgsrn.mongodb.net/${process.env.DB_BLOGS}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Call the all packages:
+const app = express()
+app.use(bodyParser.json());
+app.use(cors());
+
+// Root url:
+app.get('/', (req, res) => {
+    res.send("Hello domainamex-server, I'm ready for work.");
+})
+
+// Connect mongodb collection:
+client.connect(err => {
+    const blogsCollection = client.db(`${process.env.DB_BLOGS}`).collection(`${process.env.DB_BLOG}`);
+    console.log("Mongodb database connect okay");
+
+    // CRUD collection code ..... 
+    // .....
+});
+
+
+
+app.listen(port);
