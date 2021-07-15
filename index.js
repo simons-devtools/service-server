8c33da299bd8d540987fdd5c52a3d1ad496db0da
“c-moX” <cmoxsh@gmail.com>
2021-07-15 17:25:11 +0600

    blog 90% complete

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // Mongodb connection:
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xgsrn.mongodb.net/${process.env.DB_ECOMMERCE}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xgsrn.mongodb.net/${process.env.DB_MYDATA}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Google service account:
@@ -30,9 +30,64 @@ app.get('/', (req, res) => {
 
 // Connect mongodb collection:
 client.connect(err => {
-    const ordersCollection = client.db(`${process.env.DB_ECOMMERCE}`).collection(`${process.env.DB_ORDERS}`);
+    const blogsCollection = client.db(`${process.env.DB_MYDATA}`).collection(`${process.env.DB_BLOGS}`);
+    const themesCollection = client.db(`${process.env.DB_MYDATA}`).collection(`${process.env.DB_THEMES}`);
+    const ordersCollection = client.db(`${process.env.DB_MYDATA}`).collection(`${process.env.DB_ORDERS}`);
     console.log("Mongodb database connect okay");
 
+    // BLOGS ROUTES FUNCTIONS ----------------------------------------------------------------
+    app.post('/addBlogs', (req, res) => {
+        const newBlogs = req.body;
+        blogsCollection.insertOne(newBlogs)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    // GET all blogs from MDB cloud:
+    app.get('/blogs', (req, res) => {
+        blogsCollection.find({})
+            .toArray((err, blogs) => {
+                res.send(blogs)
+            })
+    })
+
+    // GET same package blog (by topics) from MDB cloud:
+    app.get('/bloggers/:topics', (req, res) => {
+        blogsCollection.find({ "topics": req.params.topics })
+            .toArray((err, result) => {
+                res.send(result)
+            })
+    })
+
+    // GET single blog (by _id) from MDB cloud:
+    app.get('/blog-single/:id', (req, res) => {
+        blogsCollection.find({ "_id": ObjectId(req.params.id) })
+            .toArray((err, result) => {
+                res.send(result[0])
+            })
+    })
+
+    // THEMES ROUTES FUNCTIONS ----------------------------------------------------------------
+    // POST thems to mongodb cloud:
+    app.post('/addThemes', (req, res) => {
+        const newThemes = req.body;
+        themesCollection.insertOne(newThemes)
+            .then(result => {
+                // console.log('Result=', result);
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    // GET all themes from MDB cloud:
+    app.get('/themes', (req, res) => {
+        themesCollection.find({})
+            .toArray((err, themes) => {
+                res.send(themes)
+            })
+    })
+
+    // ORDERS ROUTES FUNCTIONS ----------------------------------------------------------------
     // Post order to the MDB cloud:
     app.post('/addOrder', (req, res) => {
         const newOrder = req.body;
