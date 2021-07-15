4889c1b2b69de0d4fe6a81a493911e788a3c9906
“c-moX” <cmoxsh@gmail.com>
2021-07-15 07:10:49 +0600

    ready for work

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// const ObjectId = require('mongodb').ObjectID;
+const ObjectId = require('mongodb').ObjectID;
 const { MongoClient } = require('mongodb');
 const admin = require("firebase-admin");
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // Mongodb connection:
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xgsrn.mongodb.net/${process.env.DB_BLOGS}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xgsrn.mongodb.net/${process.env.DB_ECOMMERCE}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Google service account:
@@ -30,11 +30,17 @@ app.get('/', (req, res) => {
 
 // Connect mongodb collection:
 client.connect(err => {
-    const blogsCollection = client.db(`${process.env.DB_BLOGS}`).collection(`${process.env.DB_BLOG}`);
+    const ordersCollection = client.db(`${process.env.DB_ECOMMERCE}`).collection(`${process.env.DB_ORDERS}`);
     console.log("Mongodb database connect okay");
 
-    // CRUD collection code ..... 
-    // .....
+    // Post order to the MDB cloud:
+    app.post('/addOrder', (req, res) => {
+        const newOrder = req.body;
+        ordersCollection.insertOne(newOrder)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
 });
 
 
